1c5339efd3a8c6bdf80f4f44ea40adf9a4bb6085
augustosilveriosoares <falecomsilverio@gmail.com>
2024-07-28 14:13:57 -0300

    exercicio_rtl

---
+++ src/components/Post/index.tsx
@@ -0,0 +1,20 @@
+import styles from './Post.module.css';
+import PostComments from '../PostComments';
+import { ReactNode } from 'react';
+
+type Props = {
+    children: ReactNode;
+    imageUrl: string;
+}
+
+const Post = ({ children, imageUrl }: Props) => (
+    <div className={styles.post}>
+        <img className={styles['post-image']} src={imageUrl} alt="Post" />
+        <p className={styles['post-text']}> {children} </p>
+        <div data-testid="post-comments">
+      <PostComments />
+    </div>
+    </div>
+);
+
+export default Post;
